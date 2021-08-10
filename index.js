1ce715e0c36fc3ac99238a613eed02e6882dc291
Ruslan Sadykov <sadykovruslan91@gmail.com>
2021-08-10 14:34:27 +0300

    [CTR-4]: fixed db init feature

--- index.js
+++ index.js
@@ -10,22 +10,22 @@ const countries = require('./data/countries.json');
 const actors = require('./data/actors.json');
 const genres = require('./data/countries.json');
 
-const cityScheme = new Schema ({
+const citySchema = new Schema ({
   name: {type: String, required: true},
   countryId: {type: String, required: true}
 })
 
-const countryScheme = new Schema ({
+const countrySchema = new Schema ({
   name: {type: String, unique: true, required: true}
 })
 
-const actorScheme = new Schema ({
+const actorSchema = new Schema ({
   firstName: {type: String, required: true},
   lastName: {type: String, required: true},
   countryId: {type: String, required: true}
 })
 
-const genreScheme = new Schema ({
+const genreSchema = new Schema ({
   name: {type: String, required: true}
 })
 
@@ -42,47 +42,43 @@ const dbConnect = (url) => {
  });
 };
 
+function collectionFill(model, data, element) {
+  model.findOne({}, function (err, docs){
+    if (err) return console.log(err);
+    if (!docs) {
+      data.forEach((v)=>{
+        element = new model(v);
+        element.save(function(err){
+          if (err) return console.log(err);
+          console.log("Сохраен объект", v)
+        })
+      });
+    }
+  })
+} 
+
 app.get('/', (req, res) => {
   dbConnect(dbUrl);
 })
 
 app.get('/dbinit', (req, res) => {
   dbConnect(dbUrl);
-  const City = mongoose.model("City", cityScheme);
-  const Country = mongoose.model("Country", countryScheme);
-  const Actor = mongoose.model("Actor", actorScheme);
-  const Genre = mongoose.model("Genre", genreScheme);
+  const City = mongoose.model("City", citySchema);
+  let city;
+  collectionFill(City, cities, city);
 
-  cities.forEach((v)=>{
-    let city = new City (v);
-    city.save(function(err){   
-      if(err) return console.log(err);
-      console.log("Сохранен объект", city);
-    });
-  });
+  const Country = mongoose.model("Country", countrySchema);
+  let country;
+  collectionFill(Country, countries, country);
 
-  countries.forEach((v)=>{
-    let country = new Country (v);
-    country.save(function(err){   
-      if(err) return console.log(err);
-      console.log("Сохранен объект", country);
-    });
-  });
+  const Actor = mongoose.model("Actor", actorSchema);
+  let actor;
+  collectionFill(Actor, actors, actor);
 
-  actors.forEach((v)=>{
-    let actor = new Actor (v);
-    actor.save(function(err){   
-      if(err) return console.log(err);
-      console.log("Сохранен объект", actor);
-    });
-  });
-  genres.forEach((v)=>{
-    let genre = new Genre (v);
-    genre.save(function(err){   
-      if(err) return console.log(err);
-      console.log("Сохранен объект", genre);
-    });
-  });
+  const Genre = mongoose.model("Genre", genreSchema);
+  let genre;
+  collectionFill(Genre, genres, genre);
+
 })
 
 app.listen(port, () => {
