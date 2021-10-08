45f972dd0375b5cd26e48ae45e0059330247c4fc
Ruslan Sadykov <sadykovruslan91@gmail.com>
2021-10-08 14:15:53 +0300

    [CTR-12] Added update and delete routes for cinema, added create and delete routes for hall

--- src/routes/cinemaRoutes.js
+++ src/routes/cinemaRoutes.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const formatResponse = require("../helpers/serverResponse");
 const Cinema = require("../models/Cinema");
+const authMiddleware = require("../middleware/authMiddleware");
 
 const router = Router();
 
@@ -24,5 +25,55 @@ router.get("/cinema", async (req, res) => {
     return res.status(400).send(formatResponse(null, err, null));
   }
 });
+//update a cinema
+router.patch(
+  "/cinema",
+  authMiddleware.allowAccess(["admin"]),
+  async (req, res) => {
+    try {
+      const filter = req.body._id;
+      const update = req.body;
+      const doc = await Cinema.findOneAndUpdate({ _id: filter }, update, {
+        new: true,
+        runValidators: true,
+      });
+      if (!doc) {
+        return res
+          .status(404)
+          .send(
+            formatResponse(null, `Cinema with id ${filter} doesn't exist`, null)
+          );
+      }
+      return res
+        .status(200)
+        .send(formatResponse(doc, null, "Cinema successfully updated"));
+    } catch (err) {
+      return res.status(400).send(formatResponse(null, err, null));
+    }
+  }
+);
+//delete a cinema
+router.delete(
+  "/cinema",
+  authMiddleware.allowAccess(["admin"]),
+  async (req, res) => {
+    try {
+      const { id: _id } = req.body;
+      const cinema = await Cinema.deleteOne({ _id });
+      if (cinema.deletedCount) {
+        return res
+          .status(200)
+          .send(formatResponse(null, null, "Cinema successfully deleted"));
+      }
+      return res
+        .status(404)
+        .send(
+          formatResponse(null, `Cinema with id ${_id} doesn't exist`, null)
+        );
+    } catch (err) {
+      return res.status(400).send(formatResponse(null, err, null));
+    }
+  }
+);
 
 module.exports = router;
